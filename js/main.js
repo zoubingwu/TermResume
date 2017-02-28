cc00e86c2d19157c64d79fa6f3d89364ce950b3d
zoubingwu <zoubingwu@gmail.com>
2017-02-28 23:06:26 +0800

    added commands

--- js/main.js
+++ js/main.js
@@ -122,24 +122,34 @@
 
         returnHandler: function() {
             var ipt = document.getElementById('stdout').innerHTML;
-            var cmd = ipt.split(' ')[0];
+            var path = document.getElementsByClassName('path')[document.getElementsByClassName('path').length-1].innerHTML;
+            var cmd = ipt.split(' ')[0].toLowerCase();
+            var args = ipt.split(' ')[1] ? ipt.split(' ')[1].toLowerCase() : '';
             this.history.push(ipt);
             document.getElementById('cursor').remove();
             document.getElementById('stdout').removeAttribute('id');
             var output = document.createElement('div');
+            output.className = 'out'
             if (cmd && cmd.length) {
                 if (cmd in this.commands) {
-                    output.innerHTML = this.commands[cmd]();
+                    output.innerHTML = this.commands[cmd](args);
+                    if (cmd === 'cd' && (args == 'projects' || args == '~')) {
+                        path = args;
+                    }
+                } else {
+                    try {
+                        output.innerHTML = eval(ipt);
+                    } catch (e) {
+                        output.innerHTML = `-bash: ${cmd}: command not found`;
+                    }
                 }
             }
-            document.body.appendChild(output);
+            if(output.innerHTML !== '') document.body.appendChild(output);
             var inputTemplate =
-                `
-            <div>
-                <span>bing@<a href="https://zou.buzz" target="_blank" class="link">zou.buzz</a>:~ $ </span>
+            `<div>
+                <span><span class="name">bing</span>@<a href="https://zou.buzz" target="_blank" class="link">zou.buzz</a>:<span class="path">${path}</span> $ </span>
                 <span class="command" id="stdout"></span><span id="cursor">&nbsp;</span>
-            </div>
-            `;
+            </div>`;
             document.body.innerHTML += inputTemplate;
 
         },
@@ -151,37 +161,79 @@
             }
         },
         commands: {
-            open: function(){
+            open: function(args){
+                var args = args || '';
+                if(args == 'readme') {
+                    return `Hey there, my name is bing and welcome to my website! I'm a front-end
+developer and I'm always seeking to build awesome stuff.
 
+If you ever need help, type "help".
+
+You can contact me via <a href="mailto:zoubingwu@gmail.com" class="href">gmail</a> or check me out on Github.com<a href="https://github.com/shadeofgod" target="_blank" class="href">/shadeofgod</a>.`
+                } else if (args == 'resume'){
+                    return `123`
+                } else {
+                    return `-bash: open ${args}: No such file or directory`
+                }
             },
             ls: function() {
-
+                var path = document.getElementsByClassName('path')[document.getElementsByClassName('path').length-1].innerHTML;
+                console.log(path)
+                if( path === '~') {
+                    return `<span class="cv">README</span><span class="cv">resume</span><span class="cv">projects</span>`;
+                } else if (path === 'projects') {
+                    return `<a href="https://zou.buzz" class="href">Blog</a> - My personal blog
+<a href="https://github.com/shadeofgod" class="href">Github</a> - I have various projects hosted on my Github including this website.
+<a href="https://github.com/shadeofgod/gobang" class="href">Gobang</a> - A Gobang game with artificial intelligence written by JavaScript.
+<a href="https://github.com/shadeofgod/waterfall.js" class="href">Waterfalljs</a> - A waterfall layout library.
+<a href="https://github.com/shadeofgod/photo-gallery" class="href">PhotoGallery</a> - Photo gallery application by react.
+<a href="https://github.com/shadeofgod/2048" class="href">2048</a> - 2048 game with fully mobile support.
+<a href="https://github.com/shadeofgod/boomjs" class="href">Boomjs</a> - Funny jQuery pluggin makes your dom explode.
+<a href="https://github.com/shadeofgod/anime.js" class="href">Animejs</a> - a small frame animation library for JavaScript.
+<a href="https://github.com/shadeofgod/demos" class="href">Demos</a> - some intersting effects and demos.
+<a href="https://github.com/shadeofgod/ife/blob/master/2017/baidu_nuomi_fe/phantomjs2/task.js" class="href">phantomjs</a> - light baidu search spider by phantomjs.`;
+                }
             },
-            cd: function() {
-
+            cd: function(args) {
+                var args = args || '';
+                if (args === 'projects' || args === '~') {
+                    return '';
+                } else {
+                    return `-bash: open ${args}: No such directory`
+                }
             },
             help: function() {
-                var helpInfo = `
-                Hey there, Welcome to my website! I'm a front-end developer and <br>
-                I'm always trying to build awesome stuff.<br><br>
-                You can navigate either by clicking on anything that underlines <br>
-                when you put your mouse over it, or by typing commands in the terminal.<br>
-                Type the name of a link to view it. Use "cd" to change into a directory, or use<br>
-                "ls" to list the contents of that directory. The contents of a file can be viewed<br>
-                using "open".
-                <br><br>
-                Commands are:<br>
-                open  cd  ls  clear  help
-                <br><br>
-                If you need help, type "help".
-                <br><br>
-                You can contact me at <a href="mailto:zoubingwu@gmail.com" class="href">zoubingwu@gmail.com</a> or check me out on Github <a href="https://github.com/shadeofgod" class="href">@shadeofgod</a>.
-                <br>
-                `;
-                return helpInfo;
+                return `You can navigate either by clicking on anything that underlines
+when you put your mouse over it, or by typing commands in the terminal.
+Type the name of a link to view it. Use "profile" to change the theme and
+text color, use "cd" to change into a directory or use "ls" to list the
+contents of that directory. The contents of a file can be viewed using "open".
+
+Commands are(case insensitive):
+open  cd  ls  profile  clear  help  tree`;
             },
             profile: function(){
-
+                return `TODO`
+            },
+            clear: function(){
+                document.body.innerHTML = '';
+                return '';
+            },
+            tree: function(){
+                return `~
+|——README
+|——resume
+|——<a href="https://github.com/shadeofgod" class="link">projects</a>
+|  |——<a href="https://zou.buzz" class="href">Blog</a>
+|  |——<a href="https://github.com/shadeofgod" class="href">Github</a>
+|  |——<a href="https://github.com/shadeofgod/gobang" class="href">Gobang</a>
+|  |——<a href="https://github.com/shadeofgod/waterfall.js" class="href">Waterfalljs</a>
+|  |——<a href="https://github.com/shadeofgod/photo-gallery" class="href">PhotoGallery</a>
+|  |——<a href="https://github.com/shadeofgod/2048" class="href">2048</a>
+|  |——<a href="https://github.com/shadeofgod/boomjs" class="href">Boomjs</a>
+|  |——<a href="https://github.com/shadeofgod/anime.js" class="href">Animejs</a>
+|  |——<a href="https://github.com/shadeofgod/demos" class="href">Demos</a>
+|  |——<a href="https://github.com/shadeofgod/ife/blob/master/2017/baidu_nuomi_fe/phantomjs2/task.js" class="href">phantomjs</a>`
             }
         }
 
